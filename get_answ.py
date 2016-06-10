050cea20a1a94bf81eaa4125d1fa34f48f6b19ee
davankov <davankov@edu.hse.ru>
2016-06-10 03:16:50 +0300

    second

--- get_answ.py
+++ get_answ.py
@@ -5,13 +5,14 @@ types = {"stick": 1, "point": 2, "ring": 3, "uniform": 4}
 
 
 def read_file(train, tpe):
-    for file_name in os.listdir("data (train)/{}".format(tpe)):
-        with open("data (train)/{}/".format(tpe) + file_name, "r") as f:
+    for file_name in os.listdir("data (train)_new/{}".format(tpe)):
+        with open("data (train)_new/{}/".format(tpe) + file_name, "r") as f:
             for line in f.readlines()[1:]:
-                train.write(line.rstrip() + ", {}\n".format(types[tpe]))
+                if max(line) >= 50:
+                    train.write(line.rstrip() + ", {}\n".format(types[tpe]))
 
 
-with open("data (train)/train_sec.csv", "w") as f:
+with open("data (train)_/train_sec.csv", "w") as f:
     for tpe in types.keys():
         read_file(f, tpe)
 
--- support.py
+++ support.py
@@ -4,15 +4,15 @@ import os
 
 def fix_detector(line):
     values = line.split()
-    values[16] = str((int(values[12])+int(values[13])+int(values[17]))//3)
+    #values[16] = str((int(values[12])+int(values[13])+int(values[17]))//3)
     return ', '.join(values)
 
-for file_name in os.listdir("raw_data"):
-    with open("raw_data/" + file_name, "r") as f:
+for file_name in os.listdir("test_data"):
+    with open("test_data/" + file_name, "r") as f:
         data = [line for i, line in enumerate(f.readlines()) if i % 7 == 0]
-    with open("data/" + file_name.replace("txt", "csv"), "w") as f:
+    with open("test_data_new/" + file_name.replace("txt", "csv"), "w") as f:
         f.write(', '.join([str(number) for number in range(1, 20)]) + "\n")
         data = list(map(fix_detector, data))
-        data = [line for line in data if sum(map(int, line.replace(",", "").split())) != 0]
+        data = [line for line in data if max(map(int, line.replace(",", "").split())) >= 50]
         [f.write(line + "\n") for line in data]
     print("{} Success".format(file_name))
